bbe2be608abad56931e5d60b6a4797aff8d9e913
tanmay <tanmay.vij@softobotics.com>
2022-11-02 17:37:57 +0530

    removed commented code

--- nodejsF/src/app/app-routing.module.ts
+++ nodejsF/src/app/app-routing.module.ts
@@ -4,21 +4,11 @@ import { TaskDetailsComponent } from './task-details/task-details.component';
 
 
 const routes: Routes = [
-  // {path:'',component:DepartmentComponent},
-  // or
+ 
   { path: '', redirectTo: '/task', pathMatch: 'full' },
 
   { path: 'task', component: TaskDetailsComponent },
-//   { 
-//     path: 'departments/:id',
-//      component: DepartmentDetailsComponent,
-//      children:[
-//        {path:"overview",component:DepartmentOverviewComponent},
-//        {path:"contact",component:DepartmentContactComponent}
-//      ]
-//      },
-//   { path: 'employees', component: EmployeeComponent },
-//   { path: '**', component: PageNotFoundComponent },
+
 ];
 
 @NgModule({
